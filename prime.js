2668bb9482ff00e63eca7edf98096f6c35d65c5f
Bhusham <bhushan@gmail.com>
2022-11-02 11:38:03 +0530

    intial

---
+++ prime.js
@@ -0,0 +1,15 @@
+function CheckP(num){
+    let count = 0
+    for(let i=1; i<=num; i++){
+        if(num % i == 1){
+            count++
+        }
+    } if(count == 2){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+let num = CheckP(13);
+console.log(num)
